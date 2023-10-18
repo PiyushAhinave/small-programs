ea91897eb7895ccc3d0d34e00b855b7333c3be16
PiyushAhinave <piyushahinave@gmail.com>
2023-10-18 22:33:00 +0530

    two programs completed 1)addition of two numbers. 2)square root  of numbers

---
+++ SquareRoot.js
@@ -0,0 +1,8 @@
+//  square roots of numbers
+
+
+/*
+let a = +prompt("Enter a number : ");
+let b = Math.sqrt(a);
+console.log(b);
+*/
---
+++ addTwoNumbers.js
@@ -0,0 +1,25 @@
+//simple addition program
+/*
+let a = 5;
+let b = 15;
+let c = a+b;
+console.log(c);
+console.log(`The sum of ${a} + ${b} = ${c}`);
+*/
+
+/*
+function sum(){
+    
+    let a = +prompt("Enter first number : ");
+    let b = +prompt("Enter second number : ");
+    let c = a + b;
+    console.log(c);
+}
+*/
+
+/*
+function sum(a,b){
+    let c = a + b;
+    console.log(c);
+}
+*/
