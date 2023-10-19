0c669aa45316ef501581c4370dad57c44f33101e
PiyushAhinave <piyushahinave@gmail.com>
2023-10-19 22:25:33 +0530

    area of triangle swapping two numbers

---
+++ areaOfTriangle.js
@@ -0,0 +1,22 @@
+//area of right angle triangle
+//area = (base*height)/2
+/*
+let base = +prompt("Enter base value");
+let height = +prompt("Enter height value");
+let area = (base*height)/2;
+console.log(area);
+*/
+
+//area of any triangle
+// s = (a+b+c)/2
+//where a b c are three sides of triangle
+//area = √'s(s-a)*(s-b)*(s-c) 
+/*
+let a = +prompt("Enter first side ");
+let b = +prompt("Enter second side ");
+let c = +prompt("Enter third side ");
+let s = (a+b+c)/2;
+console.log(s);
+
+console.log("Area of triangle = "+Math.sqrt(s*(s-a)*(s-b)*(s-c)));
+*/
---
+++ swappingTwoNumber.js
@@ -0,0 +1,24 @@
+//changing two variable values
+//using temporary variable
+/*
+let a = +prompt("enter value 1");
+let b = +prompt("enter value 2");
+console.log(`value of a ${a} and value of b ${b}`);
+let temp= a;
+
+temp = a;
+a = b;
+b = temp;
+
+console.log(`value of a ${a} and value of b ${b5}`);
+*/
+
+//swapping without temporary variable
+/*
+let a = +prompt("enter value 1");
+let b = +prompt("enter value 2");
+a = a+b;
+b = a-b;
+a =a-b;
+console.log(a,b)
+*/
